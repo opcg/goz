2dc9278c42327597e6f533837758eaf304a1f322
Christopher Heckey <chrisheckey@gmail.com>
2019-08-21 08:50:43 -0700

    add test star file

--- src/hello.go
+++ src/hello.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// Execute Starlark program in a file.
 	thread := &starlark.Thread{Name: "my thread"}
-	globals, err := starlark.ExecFile(thread, "test.star", nil, nil)
+	globals, err := starlark.ExecFile(thread, "../test/test.star", nil, nil)
 	if err != nil {  }
 
 	// Retrieve a module global.
---
+++ test/test.star
@@ -0,0 +1,5 @@
+
+
+def hello():
+    print("STAR: Hello World!")
+    return "Hello World"
